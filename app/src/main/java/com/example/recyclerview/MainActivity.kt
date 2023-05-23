e839051c098f9ab5d35c1ed6cf16cd6f695eac24
RepellantHail <a20310068@ceti.mx>
2023-05-23 16:35:59 -0600

    Funcionalidad Basica

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var names: MutableList<String>
+    private lateinit var miRecyclerView: RecyclerView
+    private lateinit var miAdapter: MyAdapter
+    private lateinit var miLayoutManager: RecyclerView.LayoutManager
+    private var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        names = getAllNames()
+        miRecyclerView = findViewById(R.id.recyclerView)
+        miLayoutManager = LinearLayoutManager(this)
+        miAdapter = MyAdapter(names, R.layout.recycler_view_item, object :MyAdapter.OnItemClickListener{
+            override fun onItemClick(name: String?, position: Int) {
+                Toast.makeText(this@MainActivity, name+"-"+position, Toast.LENGTH_LONG).show()
+            }
+        })
+        miRecyclerView.setLayoutManager(miLayoutManager)
+        miRecyclerView.setAdapter(miAdapter)
+    }
+
+    fun getAllNames():MutableList<String>{
+        return object : ArrayList<String>(){
+            init{
+                add("Alejandro")
+                add("Jose")
+                add("Barrera")
+                add("Ruben")
+                add("Antonio")
+                add("Ricardo")
+                add("Veronica")
+                add("Claudia")
+                add("Ariadna")
+                add("Marcos")
+                add("Pablo")
+                add("Sofia")
+                add("Inés")
+                add("Norma")
+            }
+        }
     }
 }
